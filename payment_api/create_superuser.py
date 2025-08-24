775747ea726b9d592543f35421a1477e504a7d1e
Olaitan34 <emmfatsneh@gmail.com>
2025-08-24 09:12:23 -0700

    finaldeployment

--- payment_api/create_superuser.py
+++ payment_api/create_superuser.py
@@ -1,20 +1,18 @@
-# create_superuser.py
 import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paymentss.settings")  # <-- change to your project name
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "payment_api.settings")
 django.setup()
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "Olaitan34")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@gmail.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "12345678")
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "password123")
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
     print("Superuser created ✅")
 else:
-    print("Superuser already exists 🚀")
+    print("Superuser already exists ⚡")
